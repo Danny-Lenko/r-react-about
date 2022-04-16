b8f2e7cfa5ac8ca2f8928b7118570f3840ca97fe
DannyLenk <danylenko.14@gmail.com>
2022-04-16 12:05:26 +0300

    small fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,6 @@ import reactLogo from "../images/react-icon-small.png"
 
 export default function Navbar(props) {
     return (
-        // <nav>
-        //     <img src={reactLogo} className="nav--icon" />
-        //     <h3 className="nav--logo_text">ReactFacts</h3>
-        //     <h4 className="nav--title">React Course - Project 1</h4>
-        // </nav>
-
 
         <nav
             className={props.darkMode ? "dark" : ""}
